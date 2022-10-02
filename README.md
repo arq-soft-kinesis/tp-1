fe269e9579d0c961246bcbac1d139e95683fc495
Emiliano Prediger <epredigervianello@santandertecnologia.com.ar>
2022-10-02 01:35:24 -0300

    Agregado cluster de servicios node

--- README.md
+++ README.md
@@ -10,3 +10,18 @@ Integrantes:
 | 96454   | Caceres, Julieta    |
 | 94165   | Prediger, Emiliano  |
 | 98942   | Roselló, Jimena     |
+
+## Ejecutar servicios
+
+```bash
+$ docker-compose up -d --build --scale node=4 
+```
+
+El valor de `node` deben coincidir con los declarados en `nginx_reverse_proxy.conf` (`node` + `node/cluster`)
+
+## Rutas (pasando por nginx reverse proxy)
+
+- bbox0: `http://localhost:5555/bbox0`
+- bbox1: `http://localhost:5555/bbox1`
+- node: `http://localhost:5555/node`
+- node (cluster): `http://localhost:5555/node/cluster`
--- app/app.js
+++ app/app.js
@@ -1,29 +1,32 @@
 const express = require("express");
+const uuid = require("uuid");
 const axios = require("axios");
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 const TIMEOUT = process.env.SEGUNDOS_TIMEOUT * 1000;
+
+const appId = uuid.v4();
 
 const app = express();
 
 app.get("/ping", (req, res) => {
-    res.status(200).send("pong\n");
+    res.status(200).send(`[${appId}] pong\n`);
 });
 
-app.get("/bbox/:id", (req, res) => {
+app.get("/bbox/:id([0-1])", (req, res) => {
     const bboxId = req.params.id;
 
     axios.get(`http://nginx:80/bbox${bboxId}`)
-        .then(({ status, data }) => res.status(status).send(data))
-        .catch(() => res.status(500).send("algo feo pasó"));
+        .then(({ status, data }) => res.status(status).send(`[${appId}] ${data}`))
+        .catch(() => res.status(500).send(`[${appId}] algo feo pasó`));
 });
 
 app.get("/intensivo", (req, res) => {
     for (const t = new Date(); new Date() - t < TIMEOUT;) { }
 
-    res.status(200).send(`intensivo: done`);
+    res.status(200).send(`[${appId}] intensivo: done`);
 });
 
 app.listen(PORT, () => {
-    console.log(`Escuchando en ${PORT}`);
+    console.log(`[${appId}] Escuchando en ${PORT}`);
 });
