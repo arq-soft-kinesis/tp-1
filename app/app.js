2face17581a43394fd6d2327c438ef8a2c790cfe
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2022-11-06 19:30:34 -0300

    adding dashboard and custom metrics

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const uuid = require("uuid");
 const axios = require("axios");
+const StatsD = require('hot-shots');
 
 const PORT = process.env.PORT;
 const TIMEOUT = process.env.SEGUNDOS_TIMEOUT * 1000;
@@ -9,30 +10,46 @@ const appId = uuid.v4();
 
 const app = express();
 
+const client = new StatsD({
+    host: "graphite",
+    port: 8125
+});
+
 app.get("/ping", (req, res) => {
+    let start = new Date();
     res.status(200).send(`[${appId}] pong\n`);
+    client.timing('ping-latency', new Date() - start);
 });
 
 app.get("/bbox/:id([0-1])", (req, res) => {
     const bboxId = req.params.id;
-
+    let start = new Date();
     axios.get(`http://nginx:80/bbox${bboxId}`)
-        .then(({ status, data }) => res.status(status).send(`[${appId}] ${data}`))
+        .then(({status, data}) => res.status(status).send(`[${appId}] ${data}`))
         .catch((err) => {
             console.error(err);
             res.status(500).send(`[${appId}] algo feo pasó`);
-        });
+        }).finally(() => {
+        client.timing(`bbox-[${bboxId}]-latency`, new Date() - start);
+    });
 });
 
 app.get("/async", (req, res) => {
+    let start = new Date();
+
     setTimeout(() => {
+        client.timing(`async-latency`, new Date() - start);
         res.status(200).send(`[${appId}] async done`);
     }, TIMEOUT);
 });
 
 app.get("/intensivo", (req, res) => {
-    for (const t = new Date(); new Date() - t < TIMEOUT;) { }
+    let start = new Date();
 
+    for (const t = new Date(); new Date() - t < TIMEOUT;) {
+    }
+
+    client.timing(`intensivo-latency`, new Date() - start);
     res.status(200).send(`[${appId}] intensivo: done`);
 });
 
